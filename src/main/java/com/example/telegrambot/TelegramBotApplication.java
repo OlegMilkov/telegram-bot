2fd9ec8d78eed509982bb7268410175db99f51d9
Oleh Milkov <oleg.milkov.99@gmail.com>
2023-12-19 14:59:27 +0200

    Initial commit

---
+++ src/main/java/com/example/telegrambot/TelegramBotApplication.java
@@ -0,0 +1,20 @@
+package com.example.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotApplication.class, args);
+		System.out.println("Hellow world");
+
+int i,j;
+i=10;
+j=20;
+while(++i<--j);
+		System.out.println(i);
+	}
+
+}
